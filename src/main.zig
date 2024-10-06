2e087f72e1253500ff04b0d8fe9b54ef2dd17321
leekool <lee@imre.al>
2024-10-06 15:54:58 +1100

    remove FetchReq, getHtml includes FetchReq.get()

--- src/main.zig
+++ src/main.zig
@@ -2,60 +2,6 @@ const std = @import("std");
 const http = std.http;
 const heap = std.heap;
 
-const Client = http.Client;
-const RequestOptions = Client.RequestOptions;
-
-const FetchReq = struct {
-    const Self = @This();
-    const Allocator = std.mem.Allocator;
-
-    allocator: Allocator,
-    client: std.http.Client,
-    body: std.ArrayList(u8),
-
-    pub fn init(allocator: Allocator) Self {
-        const c = Client{ .allocator = allocator };
-        return Self{
-            .allocator = allocator,
-            .client = c,
-            .body = std.ArrayList(u8).init(allocator),
-        };
-    }
-
-    pub fn deinit(self: *Self) void {
-        self.client.deinit();
-        self.body.deinit();
-    }
-
-    pub fn get(self: *Self, url: []const u8, headers: []http.Header) !Client.FetchResult {
-        const fetch_options = Client.FetchOptions{
-            .location = Client.FetchOptions.Location{
-                .url = url,
-            },
-            .extra_headers = headers,
-            .response_storage = .{ .dynamic = &self.body },
-        };
-
-        const res = try self.client.fetch(fetch_options);
-        return res;
-    }
-
-    pub fn post(self: *Self, url: []const u8, body: []const u8, headers: []http.Header) !Client.FetchResult {
-        const fetch_options = Client.FetchOptions{
-            .location = Client.FetchOptions.Location{
-                .url = url,
-            },
-            .extra_headers = headers,
-            .method = .POST,
-            .payload = body,
-            .response_storage = .{ .dynamic = &self.body },
-        };
-
-        const res = try self.client.fetch(fetch_options);
-        return res;
-    }
-};
-
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa.deinit() == .ok);
@@ -68,11 +14,20 @@ pub fn main() !void {
 }
 
 pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
-    var req = FetchReq.init(allocator);
-    defer req.deinit();
+    var c = http.Client{ .allocator = allocator };
+    defer c.deinit();
 
-    const res = try req.get(url, &.{});
-    const body = try req.body.toOwnedSlice();
+    var body_container = std.ArrayList(u8).init(allocator);
+
+    const fetch_options = http.Client.FetchOptions{ 
+        .location = http.Client.FetchOptions.Location{
+            .url = url,
+        }, 
+        .response_storage = .{ .dynamic = &body_container } 
+    };
+
+    const res = try c.fetch(fetch_options);
+    const body = try body_container.toOwnedSlice();
 
     if (res.status != .ok) std.log.err("getHtml failed: {s}\n", .{body});
 
