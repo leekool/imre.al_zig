a9e6bd1b9cbdd14f1c615d65e75e9be012a41db7
leekool <lee@imre.al>
2024-10-07 00:37:16 +1100

    get element inner

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,20 @@ const std = @import("std");
 const http = std.http;
 const heap = std.heap;
 
+pub const Errors = error{ TagNotFound };
+
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
 
     const html = try getHtml("https://example.com", allocator);
+    std.debug.print("getHtml: {s}\n", .{html});
     defer allocator.free(html);
 
-    std.debug.print("getHtml response: {s}\n", .{html});
+
+    const div = try getElementContents(html, "a", allocator);
+    std.debug.print("getElementContents: {s}\n", .{div});
 }
 
 pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
@@ -32,4 +37,25 @@ pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
     if (res.status != .ok) std.log.err("getHtml failed: {s}\n", .{body});
 
     return body;
+}
+
+pub fn getElementContents(html: []const u8, tag: []const u8, allocator: std.mem.Allocator) ![]const u8 {
+    const tag_open: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{"<", tag});
+    defer allocator.free(tag_open);
+
+    const tag_close: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{"</", tag, ">"});
+    defer allocator.free(tag_close);
+    
+    const start_index_opt = std.mem.indexOf(u8, html, tag_open);
+    const end_index_opt = std.mem.indexOf(u8, html, tag_close);
+
+    if (start_index_opt == null or end_index_opt == null) {
+        return Errors.TagNotFound;
+    }
+    
+    const start_index = start_index_opt.? + std.mem.indexOf(u8, html[start_index_opt.?..], ">").? + 1;
+    const end_index = end_index_opt.?; 
+    
+    const substring = html[start_index..end_index];
+    return substring;
 }
