68c4be3b0d2ef2f2fada91529ce5b711b3521381
leekool <lee@imre.al>
2024-10-09 23:36:51 +1100

    getElements

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,26 @@ const std = @import("std");
 const http = std.http;
 const heap = std.heap;
 
-pub const Errors = error{ TagNotFound };
+pub const Errors = error{TagNotFound};
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(gpa.deinit() == .ok);
     const allocator = gpa.allocator();
 
-    // const html = try getHtml("https://example.com", allocator);
-    const html = try getHtml("https://maillotofc.com/products/reproduction-of-found-german-military-trainer-_-black?variant=44108846530725", allocator);
-    // std.debug.print("getHtml: {s}\n", .{html});
+    const html = try getHtml("https://example.com", allocator);
+    std.debug.print("getHtml: {s}\n", .{html});
     defer allocator.free(html);
 
+    // const div = try getElement(html, "div", allocator);
+    // std.debug.print("getElement: {s}\n", .{div});
 
-    const div = try getElementContents(html, "dd", allocator);
-    std.debug.print("getElementContents: {s}\n", .{div});
+    const divs = try getElements(html, "p", allocator);
+    defer allocator.free(divs);
+
+    for (divs) |div| {
+        std.debug.print("getElements: {s}\n", .{div});
+    }
 }
 
 pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
@@ -25,12 +30,9 @@ pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
 
     var body_container = std.ArrayList(u8).init(allocator);
 
-    const fetch_options = http.Client.FetchOptions{ 
-        .location = http.Client.FetchOptions.Location{
-            .url = url,
-        }, 
-        .response_storage = .{ .dynamic = &body_container } 
-    };
+    const fetch_options = http.Client.FetchOptions{ .location = http.Client.FetchOptions.Location{
+        .url = url,
+    }, .response_storage = .{ .dynamic = &body_container } };
 
     const res = try c.fetch(fetch_options);
     const body = try body_container.toOwnedSlice();
@@ -40,23 +42,60 @@ pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
     return body;
 }
 
-pub fn getElementContents(html: []const u8, tag: []const u8, allocator: std.mem.Allocator) ![]const u8 {
-    const tag_open: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{"<", tag});
+pub fn getElement(html: []const u8, tag: []const u8, allocator: std.mem.Allocator) ![]const u8 {
+    const tag_open: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{ "<", tag });
     defer allocator.free(tag_open);
 
-    const tag_close: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{"</", tag, ">"});
+    const tag_close: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{ "</", tag, ">" });
     defer allocator.free(tag_close);
-    
+
     const start_index_opt = std.mem.indexOf(u8, html, tag_open);
     const end_index_opt = std.mem.indexOf(u8, html, tag_close);
 
     if (start_index_opt == null or end_index_opt == null) {
         return Errors.TagNotFound;
     }
-    
+
     const start_index = start_index_opt.? + std.mem.indexOf(u8, html[start_index_opt.?..], ">").? + 1;
-    const end_index = end_index_opt.?; 
-    
+    const end_index = end_index_opt.?;
+
     const substring = html[start_index..end_index];
     return substring;
+}
+
+pub fn getElements(html: []const u8, tag: []const u8, allocator: std.mem.Allocator) ![]const []const u8 {
+    var element_list = std.ArrayList([]const u8).init(allocator);
+    defer element_list.deinit();
+
+    var search_offset: usize = 0;
+
+    while (true) {
+        const tag_open: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{"<", tag, ">"});
+        defer allocator.free(tag_open);
+
+        const tag_close: []const u8 = try std.mem.concat(allocator, u8, &[_][]const u8{"</", tag, ">"});
+        defer allocator.free(tag_close);
+
+        const start_index_opt = std.mem.indexOf(u8, html[search_offset..], tag_open);
+        if (start_index_opt == null) break;
+        
+        const start_index = start_index_opt.? + search_offset;
+
+        const tag_end_index_opt = std.mem.indexOf(u8, html[start_index..], ">");
+        if (tag_end_index_opt == null) return Errors.TagNotFound;
+
+        const tag_end_index = start_index + tag_end_index_opt.? + 1;
+
+        const end_index_opt = std.mem.indexOf(u8, html[tag_end_index..], tag_close);
+        if (end_index_opt == null) return Errors.TagNotFound;
+
+        const end_index = tag_end_index + end_index_opt.?;
+        
+        const element_content = html[tag_end_index..end_index];
+        try element_list.append(element_content);
+
+        search_offset = end_index + tag_close.len;
+    }
+
+    return element_list.toOwnedSlice();
 }
