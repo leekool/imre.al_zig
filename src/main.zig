262fad864ddfb6beb62b0ef03aedcbeafb7ae474
leekool <lee@imre.al>
2024-10-13 16:03:40 +1100

    split up html tag funcs

--- src/main.zig
+++ src/main.zig
@@ -16,12 +16,6 @@ pub fn main() !void {
     // const el = try getElement(html);
     // std.debug.print("getElement: {s}\n", .{el});
 
-    // const divs = try getElements(html, "p", allocator);
-    // defer allocator.free(divs);
-
-    // for (divs) |div| {
-    //     std.debug.print("getElements: {s}\n", .{div});
-    // }
     const elements = try getAllElements(html, allocator);
     defer allocator.free(elements);
 
@@ -48,65 +42,74 @@ pub fn getHtml(url: []const u8, allocator: std.mem.Allocator) ![]const u8 {
     return body;
 }
 
+pub fn getFirstOpeningTag(html: []const u8) ?[]const u8 {
+    var search_offset: usize = 0;
+
+    while (true) {
+        const start_opt = std.mem.indexOf(u8, html[search_offset..], "<");
+        if (start_opt == null) return null;
+
+        const start_index = start_opt.? + search_offset + 1;
+        if (html[start_index] == '/') {
+            search_offset = start_index + 1; // skip closing tags
+            continue;
+        }
+
+        const end_opt = std.mem.indexOf(u8, html[start_index..], ">");
+        if (end_opt == null) return null;
+
+        const end_index = end_opt.? + start_index;
+
+        var raw_open_tag = html[start_index..end_index];
+
+        const space_index = std.mem.indexOf(u8, raw_open_tag, " ");
+        const open_tag: []const u8 = if (space_index == null) raw_open_tag else raw_open_tag[0..space_index.?];
+
+        // std.debug.print("[getFirstOpeningTag] raw_open_tag: {s}\n", .{raw_open_tag});
+        std.debug.print("[getFirstOpeningTag] open_tag: {s}\n", .{open_tag});
+
+        return open_tag;
+    }
+}
+
+pub fn getClosingTagIndex(html: []const u8, tag_name: []const u8) ?usize {
+    var search_offset: usize = 0;
+
+    while (true) {
+        const close_start_opt = std.mem.indexOf(u8, html[search_offset..], "</");
+        if (close_start_opt == null) return null;
+
+        const close_start_index = close_start_opt.? + search_offset;
+
+        if (std.mem.startsWith(u8, html[close_start_index + 2 ..], tag_name)) {
+            return close_start_index;
+        }
+
+        search_offset = close_start_index + 1;
+    }
+}
+
 pub fn getElement(html: []const u8) ?[]const u8 {
     var search_offset: usize = 0;
 
     while (true) {
-        const open_start_opt = std.mem.indexOf(u8, html[search_offset..], "<");
-        if (open_start_opt == null) return null;
+        const open_tag_opt = getFirstOpeningTag(html[search_offset..]);
+        if (open_tag_opt == null) return null;
+        const open_tag = open_tag_opt.?;
 
-        const open_start = open_start_opt.? + search_offset + 1;
-        if (html[open_start] == '/') {
-            search_offset = open_start;
-            continue;
-        }
-
-        const open_end_opt = std.mem.indexOf(u8, html[open_start..], ">");
+        const open_end_opt = std.mem.indexOf(u8, html[search_offset..], ">");
         if (open_end_opt == null) return null;
+        const open_end = open_end_opt.? + search_offset + 1;
 
-        const open_end = open_start + open_end_opt.?;
-
-        var open_tag = html[open_start..open_end];
-        for (open_tag) |char| {
-            if (char != ' ') continue;
-
-            const char_index = std.mem.indexOfScalar(u8, open_tag, char);
-
-            if (char_index != null) {
-                open_tag = open_tag[0..char_index.?];
-            }
-        }
-        std.debug.print("[getElement] open_tag: {s}\n", .{open_tag});
-
-        // closing
-        var close_start_opt: ?usize = null;
-        var possible_offset = open_end;
-
-        while (true) {
-            close_start_opt = std.mem.indexOf(u8, html[possible_offset..], "</");
-            if (close_start_opt == null) {
-                break;
-            }
-
-            const close_start_index = close_start_opt.? + possible_offset;
-
-            if (std.mem.startsWith(u8, html[close_start_index + 2 ..], open_tag)) {
-                close_start_opt = close_start_index;
-                break;
-            }
-
-            possible_offset = close_start_index + 2;
-        }
-
-        if (close_start_opt == null) {
+        const close_index_opt = getClosingTagIndex(html[open_end..], open_tag);
+        if (close_index_opt == null) {
             search_offset = open_end + 1;
             continue;
         }
 
-        const start_index = open_end + 1;
-        const end_index = close_start_opt.?; // "</"
+        const close_start_index = close_index_opt.? + open_end;
 
-        return html[start_index..end_index];
+        return html[open_end..close_start_index];
     }
 
     return null;
@@ -119,19 +122,20 @@ pub fn getAllElements(html: []const u8, allocator: std.mem.Allocator) ![]const [
     var search_offset: usize = 0;
 
     while (true) {
-        const element = getElement(html[search_offset..]);
-        if (element != null) {
-            try element_list.append(element.?);
-        } else {
-            break;
-        }
+        const element_opt = getElement(html[search_offset..]);
+        if (element_opt == null) break;
+        
+        const element = element_opt.?; 
+        try element_list.append(element);
 
-        // std.debug.print("[getAllElements] element: {s}\n", .{element.?});
+        const open_tag_end_opt = std.mem.indexOf(u8, html[search_offset..], ">");
+        if (open_tag_end_opt == null) break;
 
-        const element_index = std.mem.lastIndexOf(u8, html, element.?);
-        search_offset = element_index.?;
+        search_offset += open_tag_end_opt.? + 1;
+
+        std.debug.print("[getAllElements] search_offset: {}, html.len: {}\n", .{search_offset, html.len});
     }
-    
+
     return element_list.toOwnedSlice();
 }
 
