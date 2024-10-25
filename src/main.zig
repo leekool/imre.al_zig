71f3b3b0293aafddc19aa3e420486bd8964ca0ea
leekool <lee@imre.al>
2024-10-26 00:30:11 +1100

    attribute_count

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,24 @@ const heap = std.heap;
 
 pub const Errors = error{ TagNotFound, NoUrl };
 
-const Element = struct { tag: []const u8, inner_html: []const u8, index: u16 };
+const Element = struct {
+    tag: []const u8,
+    index: u16,
+    inner_html: []const u8,
+    attributes: [50]Attribute,
+    attribute_count: usize,
+
+    pub fn print(self: Element) void {
+        std.debug.print("element {}:\n", .{ self.index });
+        std.debug.print("  tag: {s}\n", .{ self.tag });
+        // std.debug.print("  inner_html: {s}\n", .{self.inner_html});
+
+        std.debug.print("  attributes:\n", .{});
+        for (self.attributes[0..self.attribute_count]) |a| {
+            std.debug.print("    key: {s}, value: {s}\n", .{ a.key, a.value });
+        }
+    }
+};
 const Attribute = struct { key: []const u8, value: []const u8 };
 
 pub fn main() !void {
@@ -18,7 +35,7 @@ pub fn main() !void {
     const url = args.next() orelse return Errors.NoUrl;
     const html = try getHtml(url, allocator);
     defer allocator.free(html);
-    // std.debug.print("getHtml: {s}\n", .{html});
+    std.debug.print("getHtml: {s}\n", .{html});
 
     var t = try std.time.Timer.start();
 
@@ -28,9 +45,10 @@ pub fn main() !void {
     try getElements(html, &elements);
     // try getElementsWithAmounts(&elements);
 
-    // for (elements.items) |element| {
-    //     std.debug.print("[element]\ntag: {s}\nindex: {}\ninner_html: {s}\n", .{ element.tag, element.index, element.inner_html });
-    // }
+    for (elements.items) |element| {
+        element.print();
+        // std.debug.print("[element]\ntag: {s}\nindex: {}\ninner_html: {s}\n", .{ element.tag, element.index, element.inner_html });
+    }
 
     std.debug.print("elements.items.len: {}\nexecution time: {}\n", .{ elements.items.len, std.fmt.fmtDuration(t.read()) });
 }
@@ -97,7 +115,6 @@ pub fn getElements(html: []const u8, elements: *std.ArrayList(Element)) !void {
     for (0.., html) |i, char| {
         if (char != '<') continue;
 
-        // const tag = getFirstTag(html[i..]) orelse continue;
         const full_tag = getFirstOpeningTag(html[i..]) orelse continue;
         const tag = getElementNameFromTag(full_tag);
 
@@ -112,21 +129,23 @@ pub fn getElements(html: []const u8, elements: *std.ArrayList(Element)) !void {
 
         element_count += 1;
 
-        const attributes = getElementAttributes(full_tag);
-        _ = attributes;
-        const element = Element{ .tag = tag, .inner_html = inner, .index = element_count };
+        var attributes: [50]Attribute = undefined;
+        const attribute_count = fillElementAttributes(full_tag, &attributes);
+        // const attributes = fillElementAttributes(full_tag);
+
+        const element = Element{ .tag = tag, .index = element_count, .inner_html = inner, .attributes = attributes, .attribute_count = attribute_count };
 
         try elements.append(element);
     }
 }
 
 // todo: values should be trimmed
-pub fn getElementAttributes(start_tag: []const u8) void {
-    if (std.mem.startsWith(u8, start_tag, "script")) return; // todo: handle script tags
+pub fn fillElementAttributes(start_tag: []const u8, attributes: *[50]Attribute) usize {
+    if (std.mem.startsWith(u8, start_tag, "script")) return 0; // todo: handle script tags
 
-    std.debug.print("start_tag: {s}\n", .{start_tag});
+    // std.debug.print("start_tag: {s}\n", .{start_tag});
 
-    var attributes: [50]Attribute = undefined;
+    // var attributes: [50]Attribute = undefined;
     var attribute_count: usize = 0;
     var base_index: usize = 0;
 
@@ -144,17 +163,21 @@ pub fn getElementAttributes(start_tag: []const u8) void {
         const value_end_index = std.mem.indexOf(u8, start[value_start..], &[1]u8{value_surround_char}) orelse break;
 
         var value = start[value_start .. value_start + value_end_index];
-        value = std.mem.trim(u8, value, &[2]u8{' ', '\n'});
+        value = std.mem.trim(u8, value, &[2]u8{ ' ', '\n' });
+
+        base_index += value_start + value_end_index + 1;
+        if (value.len == 0 or key.len == 0) continue;
 
         attributes[attribute_count] = Attribute{ .key = key, .value = value };
         attribute_count += 1;
-
-        base_index += value_start + value_end_index + 1;
     }
 
-    for (attributes[0..attribute_count]) |a| {
-        std.debug.print("{s}: {s}\n", .{ a.key, a.value });
-    }
+    // for (attributes[0..attribute_count]) |a| {
+    //     std.debug.print("{s}: {s}\n", .{ a.key, a.value });
+    // }
+    return attribute_count;
+
+    // return attributes[0..attribute_count];
 }
 
 // returns what's between "<" and ">" including attributes
